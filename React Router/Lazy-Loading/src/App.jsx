69ce31eae5222ab0808129bc24b4c0399d91359a
AbhirupRoy27 <abhiruproyofficial@gmail.com>
2025-08-27 17:57:08 +0530

    Project with react-router and lazy loading

---
+++ React Router/Lazy-Loading/src/App.jsx
@@ -0,0 +1,66 @@
+import React, { lazy, Suspense } from 'react'
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
+// ... other imports
+
+// Lazily import our new homepage
+const NewHomePage = lazy(() => import('./HomePage'))
+
+function App() {
+  const navigate = useNavigate()
+  const [ishomepage, setIsHomePage] = useState(false)
+  return (
+    <>
+      {/* Wrap your Routes component with Suspense */}
+      <Suspense fallback={<div>Loading page, please wait...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <HomePage
+                navigate={navigate}
+                ishomepage={ishomepage}
+                setIsHomePage={setIsHomePage}
+              />
+            }
+          />
+          {/* Add the route for our new lazy component */}
+          <Route
+            path="/home"
+            element={
+              <NewHomePage
+                ishomepage={ishomepage}
+                setIsHomePage={setIsHomePage}
+                navigate={navigate}
+              />
+            }
+          />
+        </Routes>
+      </Suspense>
+    </>
+  )
+}
+
+export default App
+
+const HomePage = ({ navigate, ishomepage, setIsHomePage }) => {
+  return (
+    <>
+      <h1
+        onClick={() => {
+          navigate('home')
+          setIsHomePage(!ishomepage)
+        }}
+        className={ishomepage ? 'pageone' : ''}
+      >
+        Click To: Go to Home Page
+      </h1>
+    </>
+  )
+}
+
+const GoBack = () => {
+  return <></>
+}
---
+++ React Router/Lazy-Loading/src/HomePage.jsx
@@ -0,0 +1,20 @@
+function NewHomePage({ ishomepage, setIsHomePage, navigate }) {
+  return (
+    <div>
+      <h1>This is the New, Lazy-Loaded Home Page! 🚀</h1>
+      <p>This component's codes was downloaded on demand.</p>
+      {ishomepage && (
+        <h1
+          onClick={() => {
+            navigate('/')
+            setIsHomePage(!ishomepage)
+          }}
+        >
+          Go Back {'<--'}
+        </h1>
+      )}
+    </div>
+  )
+}
+
+export default NewHomePage
