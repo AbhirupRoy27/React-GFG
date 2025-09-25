0c25385e46aca4b9ea185f13c783aea4cc37451a
Abhirup Roy <abhiruproyofficial@gmail.com>
2025-09-25 17:35:02 +0530

    initial Commit: Feedback Form Project

---
+++ Feedback/src/App.jsx
@@ -0,0 +1,14 @@
+import Feedback from './pages/feedback'
+
+function App() {
+  return (
+    <>
+      <div className="bg-gray-400 p-2 shadow-2xl ">
+        <h1 className="text-2xl font-bold">Feedback Form</h1>
+      </div>
+      <Feedback />
+    </>
+  )
+}
+
+export default App
